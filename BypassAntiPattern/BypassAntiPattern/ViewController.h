66ae4cf648387799fd8ab9edc2ad51d2f626cfa2
Hai Kieu <haikieu2907@gmail.com>
2014-10-17 21:53:17 +0700

    Create bypass anti pattern project

---
+++ BypassAntiPattern/BypassAntiPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BypassAntiPattern
+//
+//  Created by Hai Kieu on 10/17/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
